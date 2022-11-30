63be7b3aaa343d667a1553e77d879fb97156eec2
maxlonergan <111542631+maxlonergan@users.noreply.github.com>
2022-11-30 10:24:49 -0700

    Update flags_seq.py

--- flags_seq.py
+++ flags_seq.py
@@ -7,7 +7,6 @@ time_start = time.perf_counter()
 flag_txt = []
 total_bytes = 0
 
-
 with open('./flags.txt', 'r') as ft:
     for line in ft:
         x = line[:-1]
@@ -15,13 +14,17 @@ with open('./flags.txt', 'r') as ft:
 
 website = 'https://www.sciencekids.co.nz/images/pictures/flags680/'
 
-
 for item in flag_txt:
     flag = item + '.jpg'
     response = requests.get(website + flag)
-    with open('./flags/'+flag, 'wb') as flag_name_file:
+    with open('./flags/flags_seq/'+flag, 'wb') as flag_name_file:
         flag_name_file.write(response.content)
-        
+        total_bytes += len(response.content)
 time_stop = time.perf_counter()
 
-print(f'Elapsed time: {time_stop-time_start} seconds')
+print(f'Elapsed time: {round(time_stop-time_start, 2)} seconds')
+print(f'{total_bytes} bytes downloaded')
+
+with open('flags_seq.out', 'w') as seq_out:
+    seq_out.writelines(f'Elapsed time: {time_stop-time_start} seconds', )
+    seq_out.writelines(f'{total_bytes} bytes downloaded')
